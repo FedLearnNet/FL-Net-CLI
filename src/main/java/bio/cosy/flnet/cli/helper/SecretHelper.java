e474c63b15fd65ecfaf614c1ddd32b55cb8bae3d
Simon Süwer <simon.suewer@uni-hamburg.de>
2026-09-23 14:21:42 +0200

    Update GitHub Actions to use latest versions of actions and improve secret generation method

--- src/main/java/bio/cosy/flnet/cli/helper/SecretHelper.java
+++ src/main/java/bio/cosy/flnet/cli/helper/SecretHelper.java
@@ -5,15 +5,15 @@ import java.security.SecureRandom;
 public final class SecretHelper {
 
     private static final String ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    private static final SecureRandom RANDOM = new SecureRandom();
 
     private SecretHelper() {
     }
 
     public static String generate(int length) {
+        SecureRandom random = new SecureRandom();
         StringBuilder secret = new StringBuilder(length);
         for (int i = 0; i < length; i++) {
-            secret.append(ALPHANUMERIC.charAt(RANDOM.nextInt(ALPHANUMERIC.length())));
+            secret.append(ALPHANUMERIC.charAt(random.nextInt(ALPHANUMERIC.length())));
         }
         return secret.toString();
     }
